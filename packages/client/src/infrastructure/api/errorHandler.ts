759a4168493f5eecd90ce0c598171ff8f518d059
Aleksandr Raykov <Aleksandr.Raykov@miningelement.com>
2023-04-18 22:26:44 +0300

    errorHandler

---
+++ packages/client/src/infrastructure/api/errorHandler.ts
@@ -0,0 +1,12 @@
+import * as HttpStatus from 'http-status-codes';
+
+export async function handleErrors(response: Response, message?: string) {
+  if (!response.ok) {
+    const error = new Error();
+    error.name = `${response.status.toString()} ${HttpStatus.getStatusText(response.status)}`;
+    error.message = message || (await response.text()) || HttpStatus.getStatusText(response.status);
+    throw error;
+  }
+
+  return response;
+}

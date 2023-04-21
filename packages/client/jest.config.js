117b9afe3c0ff0d787fb4e709bd688ca8bb512c2
nvk97 <nvk97@ya.ru>
2023-04-22 02:55:39 +0300

    add aliases reslove to jest config

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -1,10 +1,19 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@features/(.*)$': '<rootDir>/src/features/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+  },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
   },

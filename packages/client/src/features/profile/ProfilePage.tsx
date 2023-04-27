9defd6f9019693fb43bbdc3bbb7f43bf0016c406
Aleksandr Raykov <Aleksandr.Raykov@miningelement.com>
2023-04-27 15:38:05 +0300

    Исправления по ревью

--- packages/client/src/features/profile/ProfilePage.tsx
+++ packages/client/src/features/profile/ProfilePage.tsx
@@ -1,91 +1,26 @@
-import { FunctionComponent, ReactNode, useEffect, useState } from 'react';
+import { FunctionComponent } from 'react';
 import MainPageTemplate from '../../components/MainPageTemplate';
 import DataBox from '../../components/DataBox';
-import DataField from '../../components/DataField';
-import MainButton from '../../components/MainButton';
 import NavLink from '../../components/NavLink';
-import { useAppDispatch, useAppSelector } from '@/app/hooks';
-import { User, selectUser, updateProfile } from '@/app/user/userSlice';
+import { useAppSelector } from '@/app/hooks';
+import { selectUser } from '@/app/user/userSlice';
 import { Stack } from '@mui/material';
+import ProfileForm from './ProfileForm';
 
 const ProjectsPage: FunctionComponent = () => {
-  const dispatch = useAppDispatch();
-  const user = useAppSelector(selectUser);
-
-  const [first_name, setfirst_name] = useState('');
-  const [second_name, setsecond_name] = useState('');
-  const [login, setlogin] = useState('');
-  const [email, setemail] = useState('');
-  const [phone, setphone] = useState('');
-
-  useEffect(() => {
-    if (user.user) {
-      setfirst_name(user.user.first_name);
-      setsecond_name(user.user.second_name);
-      setlogin(user.user.login);
-      setemail(user.user.email);
-      setphone(user.user.phone);
-    }
-  }, [user]);
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    dispatch(
-      updateProfile({
-        first_name: first_name,
-        second_name: second_name,
-        display_name: login,
-        login: login,
-        email: email,
-        phone: phone,
-      })
-    );
-  };
-
-  const renderbase = (user: User | undefined) => {
-    if (user) {
-      return (
-        <>
-          <DataField
-            label={'name'}
-            value={user.first_name}
-            onChange={setfirst_name}></DataField>
-          <DataField
-            label={'surname'}
-            value={user.second_name}
-            onChange={setsecond_name}></DataField>
-          <DataField
-            label={'login'}
-            value={user.login}
-            onChange={setlogin}></DataField>
-          <DataField
-            label={'email'}
-            value={user.email}
-            onChange={setemail}></DataField>
-          <DataField
-            label={'phone'}
-            value={user.phone}
-            onChange={setphone}></DataField>
-        </>
-      );
-    }
-  };
+  const userState = useAppSelector(selectUser);
 
   return (
     <MainPageTemplate>
       <DataBox>
-        <form onSubmit={(event) => handleSubmit(event)}>
-          {user.user && renderbase(user.user)}
-          <Stack
-            margin="auto"
-            direction="column"
-            justifyContent="center"
-            alignItems="center">
-            <MainButton label="Save" />
-            <NavLink href="/">cancel</NavLink>
-          </Stack>
-        </form>
+        <Stack
+          margin="auto"
+          direction="column"
+          justifyContent="center"
+          alignItems="center">
+          <ProfileForm user={userState.user}></ProfileForm>
+          <NavLink href="/">cancel</NavLink>
+        </Stack>
       </DataBox>
     </MainPageTemplate>
   );

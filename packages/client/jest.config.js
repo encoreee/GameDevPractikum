62caa32d6d34e5f393b4d3fa0ba1903a51d49653
nvk97 <nvk97@ya.ru>
2023-05-01 21:27:03 +0300

    fix test errors

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -7,14 +7,14 @@ export default {
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$':
+      'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@app/(.*)$': '<rootDir>/src/app/$1',
     '^@assets/(.*)$': '<rootDir>/src/assets/$1',
     '^@components/(.*)$': '<rootDir>/src/components/$1',
     '^@features/(.*)$': '<rootDir>/src/features/$1',
     '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$':
-      'identity-obj-proxy',
   },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,

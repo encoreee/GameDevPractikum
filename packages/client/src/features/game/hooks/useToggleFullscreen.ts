8554a4dfb3de3d277a524a5c8276eaf405622eef
kirei-io <44925378+kirei-io@users.noreply.github.com>
2023-05-16 20:53:03 +0300

    chore: fullscreen api hook

---
+++ packages/client/src/features/game/hooks/useToggleFullscreen.ts
@@ -0,0 +1,41 @@
+import { useRef, useEffect } from 'react';
+
+/**
+ * FullScreen hook. Toggle full screen `div` element. Must be bound to the `ref` of the required div element
+ * @param key `typeof KeyboardEvent.key`
+ * @returns HTMLDivElement
+ */
+export const useToggleFullScreen = (key: string) => {
+  const element = useRef<HTMLDivElement>(null);
+
+  const toggleFullScreen = async () => {
+    if (element.current === null) {
+      throw new Error('Element is not defined');
+    }
+    if (!document.fullscreenElement) {
+      await element.current.requestFullscreen();
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  };
+
+  useEffect(() => {
+    if (element.current !== null) {
+      document.addEventListener('keydown', onKeyDownHandler);
+    }
+
+    return () => {
+      document.removeEventListener('keydown', onKeyDownHandler);
+    };
+  }, [element]);
+
+  const onKeyDownHandler = (event: KeyboardEvent) => {
+    if (event.altKey || event.ctrlKey) return;
+
+    if (event.key === key) {
+      toggleFullScreen();
+    }
+  };
+
+  return element;
+};

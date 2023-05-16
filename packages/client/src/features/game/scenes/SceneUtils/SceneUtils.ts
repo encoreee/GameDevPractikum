d4fb74658e13ced25c63ec8c4b341c561d8523a2
Aleksandr Raykov <Aleksandr.Raykov@miningelement.com>
2023-05-16 12:36:25 +0300

    Корректировка положений меток

--- packages/client/src/features/game/scenes/SceneUtils/SceneUtils.ts
+++ packages/client/src/features/game/scenes/SceneUtils/SceneUtils.ts
@@ -8,7 +8,7 @@ import { Vector2 } from '../../utils/Vector2';
 export function createPlayerPoint(profile: PlayerProfile): ReferenceObject {
   return new ReferenceObject(
     profile.points.toString(),
-    new Vector2(10, 30),
+    new Vector2(10, 40),
     new ReferenceObjectAction(),
     new ReferenceObjectGraphics('blue', 23)
   );
@@ -17,7 +17,7 @@ export function createPlayerPoint(profile: PlayerProfile): ReferenceObject {
 export function createLabel(text: string, canvasSize: Size): ReferenceObject {
   return new ReferenceObject(
     text,
-    new Vector2(canvasSize.width / 2 - 50, canvasSize.height / 2),
+    new Vector2(canvasSize.width / 2 - 125, canvasSize.height / 2 - 25),
     new ReferenceObjectAction(),
     new ReferenceObjectGraphics('white', 30)
   );

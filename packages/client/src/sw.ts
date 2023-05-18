b8d715ede70039953992f57a67dfc5424eeb8746
dmitrii-yp <da.mcarov@ya.ru>
2023-05-18 16:02:05 +0300

    change timeout

--- packages/client/src/sw.ts
+++ packages/client/src/sw.ts
@@ -2,7 +2,7 @@ declare const self: ServiceWorkerGlobalScope;
 
 const CACHE_NAME = 'app-cache-v1';
 const URLS = ['/index.html', '/app.js', '/assets/background.png', '/'];
-const TIMEOUT = 400;
+const TIMEOUT = 1000;
 
 const fromNetwork = (request: Request): Promise<Response> => {
   return new Promise((fulfill, reject) => {

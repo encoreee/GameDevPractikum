d40799f80adedec016a4aeb8d2d033ca8f586b7a
nvk97 <nvk97@ya.ru>
2023-05-24 09:23:49 +0300

    merge

--- packages/client/src/features/game/components/GameCanvas.tsx
+++ packages/client/src/features/game/components/GameCanvas.tsx
@@ -7,6 +7,7 @@ import { Canvas as EngineCanvas } from '../core/Canvas';
 import { playerConfig } from '../Config';
 import Audio from '@features/Audio';
 import { PlayerProfile } from '../scenes/SceneUtils/PlayerUtils';
+import { useNavigate } from 'react-router-dom';
 
 const Canvas = styled.canvas`
   border: 1px solid silver;
@@ -31,6 +32,14 @@ const galaga = new GalagaGame(dummy);
 const GameCanvas: React.FC = () => {
   const canvas = useRef<HTMLCanvasElement>(null);
   const fullScreenElement = useToggleFullScreen(KEY_BINDINGS.fullScreen);
+
+  const navigate = useNavigate();
+
+  const onEndGame = () => {
+    navigate('/game-over');
+  };
+
+  galaga.onendgame = onEndGame;
 
   useEffect(() => {
     if (canvas.current !== null) {

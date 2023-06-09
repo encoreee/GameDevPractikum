96462d0450ae8c0a75415434611c0797f6ddb2cc
OluwadahunsiA <tommydate0@gmail.com>
2023-06-09 16:35:04 +0300

    emm/gam-41

---
+++ packages/client/src/fetchAsync.ts
@@ -0,0 +1,21 @@
+import { apiFetch, API_ADDRESS } from '@infrastructure/apiFetch';
+import fetch from 'node-fetch';
+
+const requestOptions = {
+  method: 'GET',
+  credentials: 'include',
+};
+
+requestOptions.credentials = 'include';
+
+export async function fetchAsync(callback: (arg0: any) => any) {
+  try {
+    const res = await fetch(`${API_ADDRESS}/auth/user`, requestOptions);
+
+    const data = await res.json();
+
+    return callback(data);
+  } catch (e) {
+    console.log(e);
+  }
+}

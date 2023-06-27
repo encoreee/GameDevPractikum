474d80a2d8b17065fbade4a8f3f83b8a73f47b9c
dmitrii-yp <da.mcarov@ya.ru>
2023-06-27 14:31:03 +0300

    fix issues after merging

--- packages/server/app/sequelize.ts
+++ packages/server/app/sequelize.ts
@@ -1,30 +1,7 @@
 import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
-<<<<<<< HEAD
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelizeOptions: SequelizeOptions = {
-  host: 'localhost',
-  port: 5433,
-  username: 'postgres',
-  password: 'eCLLQZYsM9&',
-  database: 'postgres',
-  dialect: 'postgres',
-};
-
-// const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
-//   process.env;
-
-// const sequelizeOptions: SequelizeOptions = {
-//   host: 'localhost',
-//   port: Number.parseInt(POSTGRES_PORT ?? '5432'),
-//   username: POSTGRES_USER,
-//   password: POSTGRES_PASSWORD,
-//   database: POSTGRES_DB,
-//   dialect: 'postgres',
-// };
-
-=======
 import {
   POSTGRES_USER,
   POSTGRES_PASSWORD,
@@ -42,7 +19,6 @@ const sequelizeOptions: SequelizeOptions = {
   dialect: 'postgres',
 };
 
->>>>>>> origin/sprint_4
 const sequelize = new Sequelize(sequelizeOptions);
 
 export default sequelize;

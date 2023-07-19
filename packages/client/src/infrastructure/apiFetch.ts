bf05625642fe537e80b751f71621cd8418a4c147
dmitrii-yp <da.mcarov@ya.ru>
2023-07-19 16:38:32 +0300

    return prod server addresses

--- packages/client/src/infrastructure/apiFetch.ts
+++ packages/client/src/infrastructure/apiFetch.ts
@@ -1,5 +1,5 @@
-export const API_ADDRESS = 'https://ya-praktikum.tech/api/v2';
-export const LOCAL_ADDRESS = 'http://localhost:3000';
+export const API_ADDRESS = 'https://galagagame.ya-praktikum.tech/api/v2';
+export const LOCAL_ADDRESS = 'https://galagagame.ya-praktikum.tech';
 
 export function apiFetch() {
   const request = (method: string) => {
